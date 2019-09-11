639110928ae06642d384b6afd75e6a64b44efa51
gelinas <jlgelinas@gmail.com>
2019-09-10 23:37:49 -0500

    MVP plus stretch

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,34 @@
 import React from "react";
 
-const NumberButton = () => {
-  return (
+const NumberButton = (props) => {
+  console.log(`numberbutton props are: ${props.number} `);
+  console.log(`numberbutton props are: ${props.press} `);
+
+
+  if (props.number === "0") {
+    return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <div 
+        className="numbers-button-zero" 
+        onClick={() => props.press(props.number)}
+      >
+        { props.number }
+      </div>
     </>
-  );
+    );
+  } else {
+    return (
+      <>
+        <div 
+          className="numbers-button"
+          onClick={() => props.press(props.number)}
+        >
+          { props.number }</div>
+        {/* Display a button element rendering the data being passed down from the parent container on props */}
+      </>
+      );
+  }
+
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
 
+import SpecialButton from './SpecialButton';
+
 //Import your array data to from the provided data file
+
+import { specials } from '../../../data.js'
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialsState, setSpecialsState] = useState(specials);
 
   return (
-    <div>
+    <div className="specials">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       specials.map((el) => (<SpecialButton special={el} />))
+
+       }
     </div>
   );
 };
+
+export default Specials;
